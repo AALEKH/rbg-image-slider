22342afbe33021a0ec38894edd9ae6600bdecf1f
Aalekh Nigam <aalekh1993@rediffmail.com>
2013-11-03 14:30:43 +0530

    Update jquery.rbgimageslider.js
    Improved code

--- jquery.rbgimageslider.js
+++ jquery.rbgimageslider.js
@@ -1,6 +1,8 @@
 (function($){
   
-  $.rbgimageslider=function(){
+  $.fn.rbgimageslider=function(){
+ 
+    
   var counter=0; 
   var para=new Array();
   var len=arguments.length;
@@ -13,33 +15,79 @@
     
     return this.each(function(){
     
+     
+      $('#next2').click(function(){
+      
+      $('#aalekhimage').toggle(function(){
     
-
-    $("#next2").click(function(){
     var adi=document.getElementById('aalekhimage');
-    var adi2=document.getElementById('next1');
-    var adi5=document.getElementById('aalekh2');
-    adi5.style.width="900px";  
+    var adi7=document.getElementById('fullscreen');   
+    var adi5=document.getElementById('prev');
+    adi5.style.top="623px";
+    adi5.style.right="920px";   
     adi.style.height="600px";
-    adi.style.width="800px"; 
-    adi2.style.marginLeft="300px";
+    adi.style.width="900px";
+    adi7.style.marginLeft="600px";   
     var adi3=document.getElementById('next2');
     var adi4=document.getElementById('next');  
-    adi3.style.marginLeft="26px";
-    adi4.style.marginLeft="340px"; 
+    adi3.style.top="623px";
+    adi3.style.right="540px";
+    adi4.style.top="623px";
+    adi4.style.right="100px";   
     var adi6=document.getElementById('fullscreen');
     adi6.style.marginLeft="600px";  
 
-});
+    var elem = document.getElementById('fullscreen');  
+    if(document.webkitFullscreenElement) {  
+      document.webkitCancelFullScreen(); 
+      }  
+    else {  
+            elem.webkitRequestFullScreen();  
+          } 
+        
+      },function(){
       
+    var adi=document.getElementById('aalekhimage');
+    var adi7=document.getElementById('fullscreen');   
+    var adi5=document.getElementById('prev');
+    adi5.style.top="320px";
+    adi5.style.right="420px";   
+    adi.style.height="300px";
+    adi.style.width="400px";
+    adi7.style.marginLeft="0px";   
+    var adi3=document.getElementById('next2');
+    var adi4=document.getElementById('next');  
+    adi3.style.top="320px";
+    adi3.style.right="270px";
+    adi4.style.top="320px";
+    adi4.style.right="100px";   
+    var adi6=document.getElementById('fullscreen');
+    adi6.style.marginLeft="0px";  
+
+    var elem = document.getElementById('fullscreen');  
+    if(document.webkitFullscreenElement) {  
+      document.webkitCancelFullScreen(); 
+      }  
+    else {  
+            elem.webkitRequestFullScreen();  
+          }
+        
+      });
+      });
+     
+
+     
+      $('.back').click(function() {
+       $('#aalekhimage').fadeOut(200);
+       });
       
-      $('#next').click(function(){
-      
+      $('.back').click(function(){
+       
         if(counter >= len){
          counter=0;
          
          a.src=para[counter]; 
-          
+         counter++; 
         }  
         else{
          
@@ -47,39 +95,43 @@
          counter++;
           
         }
+      
         
       });
+
+  $('.back').click(function() {
+$('#aalekhimage').fadeIn(200);
+});
       
-      $('.back').click(function(){
+      $('#next').click(function() {
+        $('#aalekhimage').fadeOut('fast');
+        });
+      $('#next').click(function(){
+        
+        
         if(counter < 0){
          counter=len-1; 
          
-         a.src=para[counter]; 
+         a.src=para[counter];
+         counter--; 
         }
         else{
         
         a.src=para[counter];
         counter--;
         }
-        
+       
       });
-      
-      
-      $('#next2').on('click', function(){  
-        var elem = document.getElementById('fullscreen');  
-        if(document.webkitFullscreenElement) {  
-            document.webkitCancelFullScreen(); 
-        }  
-        else {  
-            elem.webkitRequestFullScreen();  
-             } 
-         }); 
-      
-      
-      
+       $('#next').click(function() {
+        $('#aalekhimage').fadeIn('fast');
 });
+       
+      
+      
+      
+      
     
-    
+    }); 
     
   }
 
